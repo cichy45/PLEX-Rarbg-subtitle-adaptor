26360fc93c825f78a14b1a21c4c0980678ea8e55
cichy45 <86913474+cichy45@users.noreply.github.com>
2022-04-26 00:10:55 +0200

    Add files via upload

--- adapt.ps1
+++ adapt.ps1
@@ -5,29 +5,33 @@ $filePath = Get-Location
 $files = Get-ChildItem -File -Recurse "$filePath"
 
             foreach($file in $files)  {     
-            if($file.FullName -like "*English*") {
+            if($file.FullName -like "*English.srt") {
 				if($file.FullName -NotLike "*English.eng*") {
                 Rename-Item $file.FullName -NewName "$($file.BaseName+".eng"+($file.extension))"
 				Write-Host "Renaming English subs in: " $file.DirectoryName
 				}
             }
-			elseif($file.FullName -like "*Polish*") {
+			elseif($file.FullName -like "*Polish.srt") {
 				if($file.FullName -NotLike "*Polish.pol*") {
                 Rename-Item $file.FullName -NewName "$($file.BaseName+".pol"+($file.extension))" 
 				Write-Host "Renaming Polish subs in: " $file.DirectoryName
 				}
             }
-			elseif($file.FullName -like "*German*") {
+			elseif($file.FullName -like "*German.srt") {
 				if($file.FullName -NotLike "*German.ger*") {
                 Rename-Item $file.FullName -NewName "$($file.BaseName+".ger"+($file.extension))"
 				Write-Host "Renaming German subs in: " $file.DirectoryName				
 				}
             }
-			else {
-				if($file.Extension -like "*.srt") {
+			else{
+				if($file.Extension -Like "*.srt") {
+                    if($file.Name -NotLike "*eng.srt") {
+                    if($file.Name -NotLike "*pol.srt") {
+                    if($file.Name -NotLike "*ger.srt") {
 					Remove-Item -Recurse -Path $file.FullName -Force
 					Write-Host "Deleting non eng/ger/pol subs in: " $file.DirectoryName
-				}
+				    }}}
+                }
 				if($file.Extension -like "*.exe") {
 					Remove-Item -Recurse -Path $file.FullName -Force
 					Write-Host "Deleting exe in: " $file.DirectoryName
